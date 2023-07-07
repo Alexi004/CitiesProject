f57da6987e143c7055a2ff3a5fa6131dcb8f8b01
vlshutyak <vl.shutyak@gmail.com>
2023-07-07 21:11:28 +0300

    player class

---
+++ src/main/java/Player.java
@@ -0,0 +1,15 @@
+public class Player {
+    private String playerName;
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public Player(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+}
