e3b8f4f07b185d8ce626c960fb8407863aa447a7
vlshutyak <vl.shutyak@gmail.com>
2023-07-11 23:31:26 +0300

    GameWindow - fix ai answer

--- src/main/java/window/GameWindow.java
+++ src/main/java/window/GameWindow.java
@@ -32,9 +32,19 @@ public class GameWindow extends JFrame {
             boolean isCityAvailable = game.isCityAvailable(userInput);
 
             if (isCityAvailable) {
-                String aiCity = game.getRandomCity(userInput.charAt(userInput.length() - 1));
+                String aiCity = game.getRandomCity(game.getLastCitySymbol());
+                if (aiCity.equalsIgnoreCase("not found")){
+                    ComponentsGameWindow.showMessage("AI loss at turn"+game.getLastCitySymbol(),"AI loss");
+                    return;
+                }else {
+                    isCityAvailable = game.isCityAvailable(aiCity);
+                    if (!isCityAvailable){
+                        ComponentsGameWindow.showMessage("AI loss at turn !!! "+game.getLastCitySymbol(),"AI loss");
+                        return;
+                    }
+                }
                 ComponentsGameWindow.setComputerResponse(aiCity.substring(0, 1).toUpperCase() + aiCity.substring(1));
-                game.addCity(userInput, aiCity);
+                //game.addCity(userInput, aiCity);
             } else {
                 ComponentsGameWindow.showMessage("Такого міста нема","Міста нема");
             }
